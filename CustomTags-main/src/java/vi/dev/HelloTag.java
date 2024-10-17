bc626955fe3e1a2b4b27d5bea572110b067af8ac
tuongvi5091 <tuongvi5091@gmail.com>
2024-10-17 09:02:05 +0700

    CustomTags-main

---
+++ CustomTags-main/src/java/vi/dev/HelloTag.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package vi.dev;
+
+import jakarta.servlet.jsp.tagext.*;
+import jakarta.servlet.jsp.*;
+import java.io.*;
+
+public class HelloTag extends SimpleTagSupport {
+   public void doTag() throws JspException, IOException {
+      JspWriter out = getJspContext().getOut();
+      out.println("Hello Custom Tag!");
+   }
+}
